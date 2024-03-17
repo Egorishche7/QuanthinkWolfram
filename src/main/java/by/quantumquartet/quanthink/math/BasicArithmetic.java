6d6d99b9e52d571c769bc5cd6fa9a09e0f5da438
Overwees <pascha.kostetskii@gamil.com>
2024-03-17 21:28:34 +0300

    Добавил пару тест кейсов, исправил баг со степенью, разделил операции по функциям

--- src/main/java/by/quantumquartet/quanthink/math/BasicArithmetic.java
+++ src/main/java/by/quantumquartet/quanthink/math/BasicArithmetic.java
@@ -2,28 +2,23 @@ package by.quantumquartet.quanthink.math;
 
 public class BasicArithmetic {
 
-    private final static String[] order = {"(", "^", "*", "+"};
+    private final static String[] order = {"e","(", "^", "*", "+"};
     private final static char PI = 'π';
     private final static char EXP = 'e';
     private static Boolean checkDouble(double value){
-        if (Math.floor(value) - value == 0)
-            return false;
-        return true;
+        return Math.floor(value) - value != 0;
     }
 
     public static String SolveExpression(String expr){
         String tmp = expr;
-        for (int j = 0; j < order.length; j++){
-            switch (j){
-                case 0:
-                    while (true)
-                    {
+        for (String s : order) {
+            switch (s) {
+                case "(":
+                    while (true) {
                         int begin = 0;
                         int end = 0;
-                        for (int i = 0; i < tmp.length(); i++)
-                        {
-                            if (tmp.charAt(i) == '(')
-                            {
+                        for (int i = 0; i < tmp.length(); i++) {
+                            if (tmp.charAt(i) == '(') {
                                 begin = i;
                                 continue;
                             }
@@ -35,171 +30,21 @@ public class BasicArithmetic {
                         if (begin == end)
                             break;
                         else
-                            tmp = tmp.substring(0,begin) + SolveExpression(tmp.substring(begin + 1,end))
+                            tmp = tmp.substring(0, begin) + SolveExpression(tmp.substring(begin + 1, end))
                                     + tmp.substring(end + 1);
                     }
                     break;
-                case 1:
-                    while (true)
-                    {
-                        int ind_operation_begin = 0;
-                        Boolean operation_begin = false;
-                        int ind_operation_end = 0;
-                        int check_pow = 0;
-                        for (int i = 0; i < tmp.length(); i++) {
-                            if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' || tmp.charAt(i) == '*' || tmp.charAt(i) == '/')
-                            {
-                                ind_operation_begin = i;
-                                operation_begin = true;
-                                continue;
-                            }
-                            if (tmp.charAt(i) == '^'){
-                                check_pow = i;
-                                break;
-                            }
-
-                        }
-                        if (check_pow == 0)
-                            break;
-                        else{
-                            for (int i = check_pow; i < tmp.length(); i++) {
-                                if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' || tmp.charAt(i) == '*' || tmp.charAt(i) == '/')
-                                {
-                                    if((tmp.charAt(i) == '+' || tmp.charAt(i) == '-') && i - 1 == check_pow)
-                                        continue;
-                                    ind_operation_end = i;
-                                    break;
-                                }
-                            }
-                            if (ind_operation_end == 0)
-                                ind_operation_end = tmp.length();
-                            double basis = 0;
-                            if (operation_begin)
-                                basis = Double.parseDouble(tmp.substring(ind_operation_begin + 1, check_pow));
-                            else
-                                basis = Double.parseDouble(tmp.substring(ind_operation_begin, check_pow));
-                            double degree = Double.parseDouble(tmp.substring(check_pow + 1, ind_operation_end));
-                            if(checkDouble(Math.pow(basis, degree)))
-                                tmp = tmp.substring(0,ind_operation_begin) + Double.toString(Math.pow(basis, degree))
-                                        + tmp.substring(ind_operation_end);
-                            else
-                                tmp = tmp.substring(0,ind_operation_begin) + Integer.toString((int)Math.pow(basis, degree))
-                                        + tmp.substring(ind_operation_end);
-                        }
-                    }
+                case "e":
+                    tmp = ConvertConstToValues(tmp);
                     break;
-                case 2:
-                    while (true)
-                    {
-                        int ind_operation_begin = 0;
-                        Boolean operation_begin = false;
-                        int ind_operation_end = 0;
-                        int check_mul_del = 0;
-                        for (int i = 0; i < tmp.length(); i++) {
-                            if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' )
-                            {
-                                ind_operation_begin = i;
-                                operation_begin = true;
-                                continue;
-                            }
-                            if (tmp.charAt(i) == '*' || tmp.charAt(i) == '/'){
-                                check_mul_del = i;
-                                break;
-                            }
-
-                        }
-                        if (check_mul_del == 0)
-                            break;
-                        else{
-                            for (int i = check_mul_del; i < tmp.length(); i++) {
-                                if((tmp.charAt(i) == '+' || tmp.charAt(i) == '-')) {
-                                    if (i - 1 == check_mul_del)
-                                        continue;
-                                    ind_operation_end = i;
-                                    break;
-                                }
-                            }
-                            if (ind_operation_end == 0)
-                                ind_operation_end = tmp.length();
-                            double left = 0;
-                            if (operation_begin)
-                                left = Double.parseDouble(tmp.substring(ind_operation_begin + 1, check_mul_del));
-                            else
-                                left = Double.parseDouble(tmp.substring(ind_operation_begin, check_mul_del));
-                            double right = Double.parseDouble(tmp.substring(check_mul_del + 1, ind_operation_end));
-                            if (tmp.charAt(check_mul_del) == '*')
-                            {
-                                if(checkDouble(left * right))
-                                    tmp = tmp.substring(0,ind_operation_begin) + Double.toString(left * right)
-                                            + tmp.substring(ind_operation_end);
-                                else
-                                    tmp = tmp.substring(0,ind_operation_begin) + Integer.toString((int)(left * right))
-                                            + tmp.substring(ind_operation_end);
-                            }
-                            else {
-                                if(checkDouble(left / right))
-                                    tmp = tmp.substring(0,ind_operation_begin) + Double.toString(left / right)
-                                            + tmp.substring(ind_operation_end);
-                                else
-                                    tmp = tmp.substring(0,ind_operation_begin) + Integer.toString((int)(left /right))
-                                            + tmp.substring(ind_operation_end);
-                            }
-
-                        }
-                    }
+                case "^":
+                    tmp = SolvePow(tmp);
                     break;
-                case 3:
-                    while (true)
-                    {
-                        int ind_operation_begin = 0;
-                        int ind_operation_end = 0;
-                        int check_sum_sub = 0;
-                        for (int i = 0; i < tmp.length(); i++) {
-                            if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' )
-                            {
-                                if (i == 0)
-                                    continue;
-                                check_sum_sub = i;
-                                break;
-                            }
-
-                        }
-                        if (check_sum_sub == 0)
-                            break;
-                        else{
-                            for (int i = check_sum_sub + 1; i < tmp.length(); i++) {
-                                if((tmp.charAt(i) == '+' || tmp.charAt(i) == '-')) {
-                                    if (i - 1 == check_sum_sub)
-                                        continue;
-                                    ind_operation_end = i;
-                                    break;
-                                }
-                            }
-                            if (ind_operation_end == 0)
-                                ind_operation_end = tmp.length();
-                            double left = 0;
-                            left = Double.parseDouble(tmp.substring(ind_operation_begin, check_sum_sub));
-                            double right = Double.parseDouble(tmp.substring(check_sum_sub + 1));
-                            if (tmp.charAt(check_sum_sub) == '+')
-                            {
-                                if(checkDouble(left * right))
-                                    tmp = tmp.substring(0,ind_operation_begin) + Double.toString(left + right)
-                                            + tmp.substring(ind_operation_end);
-                                else
-                                    tmp = tmp.substring(0,ind_operation_begin) + Integer.toString((int)(left + right))
-                                            + tmp.substring(ind_operation_end);
-                            }
-                            else {
-                                if(checkDouble(left / right))
-                                    tmp = tmp.substring(0,ind_operation_begin) + Double.toString(left - right)
-                                            + tmp.substring(ind_operation_end);
-                                else
-                                    tmp = tmp.substring(0,ind_operation_begin) + Integer.toString((int)(left - right))
-                                            + tmp.substring(ind_operation_end);
-                            }
-
-                        }
-                    }
+                case "*":
+                    tmp = SolveMulDiv(tmp);
+                    break;
+                case "+":
+                    tmp = SolveSumSub(tmp);
                     break;
             }
         }
@@ -209,4 +54,177 @@ public class BasicArithmetic {
             return String.valueOf((int)Double.parseDouble(tmp));
     }
 
+    private static String ConvertConstToValues(String expr){
+        String tmp = expr;
+        while (true)
+        {
+            int index = tmp.indexOf(PI);
+            if (index == -1)
+                break;
+            tmp = tmp.substring(0, index) + Math.PI + tmp.substring(index + 1);
+        }
+        while (true)
+        {
+            int index = tmp.indexOf(EXP);
+            if (index == -1)
+                break;
+            tmp = tmp.substring(0, index) + Math.E + tmp.substring(index + 1);
+        }
+        return tmp;
+    }
+
+    private static String SolveMulDiv(String expr) {
+        String tmp = expr;
+        while (true)
+        {
+            int ind_operation_begin = 0;
+            boolean operation_begin = false;
+            int ind_operation_end = 0;
+            int check_mul_del = 0;
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' )
+                {
+                    ind_operation_begin = i;
+                    operation_begin = true;
+                    continue;
+                }
+                if (tmp.charAt(i) == '*' || tmp.charAt(i) == '/'){
+                    check_mul_del = i;
+                    break;
+                }
+
+            }
+            if (check_mul_del == 0)
+                break;
+            else{
+                for (int i = check_mul_del; i < tmp.length(); i++) {
+                    if((tmp.charAt(i) == '+' || tmp.charAt(i) == '-')) {
+                        if (i - 1 == check_mul_del)
+                            continue;
+                        ind_operation_end = i;
+                        break;
+                    }
+                }
+                if (ind_operation_end == 0)
+                    ind_operation_end = tmp.length();
+                double left;
+                if (operation_begin) {
+                    left = Double.parseDouble(tmp.substring(ind_operation_begin + 1, check_mul_del));
+                    ind_operation_begin++;
+                }
+                else
+                    left = Double.parseDouble(tmp.substring(ind_operation_begin, check_mul_del));
+                double right = Double.parseDouble(tmp.substring(check_mul_del + 1, ind_operation_end));
+                if (tmp.charAt(check_mul_del) == '*')
+                {
+                    tmp = tmp.substring(0,ind_operation_begin) + (left * right)
+                            + tmp.substring(ind_operation_end);
+                }
+                else {
+                    tmp = tmp.substring(0,ind_operation_begin) + (left / right)
+                            + tmp.substring(ind_operation_end);
+                }
+
+            }
+        }
+        return tmp;
+    }
+
+    private static String SolvePow(String expr){
+        String tmp = expr;
+        while (true)
+        {
+            int ind_operation_begin = 0;
+            boolean operation_begin = false;
+            int ind_operation_end = 0;
+            int check_pow = 0;
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' || tmp.charAt(i) == '*' || tmp.charAt(i) == '/')
+                {
+                    ind_operation_begin = i;
+                    operation_begin = true;
+                    continue;
+                }
+                if (tmp.charAt(i) == '^'){
+                    check_pow = i;
+                    break;
+                }
+
+            }
+            if (check_pow == 0)
+                break;
+            else{
+                for (int i = check_pow; i < tmp.length(); i++) {
+                    if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' || tmp.charAt(i) == '*' || tmp.charAt(i) == '/')
+                    {
+                        if((tmp.charAt(i) == '+' || tmp.charAt(i) == '-') && i - 1 == check_pow)
+                            continue;
+                        ind_operation_end = i;
+                        break;
+                    }
+                }
+                if (ind_operation_end == 0)
+                    ind_operation_end = tmp.length();
+                double basis;
+                if (operation_begin) {
+                    basis = Double.parseDouble(tmp.substring(ind_operation_begin + 1, check_pow));
+                    ind_operation_begin++;
+                }
+                else
+                    basis = Double.parseDouble(tmp.substring(ind_operation_begin, check_pow));
+                double degree = Double.parseDouble(tmp.substring(check_pow + 1, ind_operation_end));
+                tmp = tmp.substring(0,ind_operation_begin) + Math.pow(basis, degree)
+                        + tmp.substring(ind_operation_end);
+            }
+        }
+        return tmp;
+    }
+
+    private static String SolveSumSub(String expr){
+        String tmp = expr;
+        while (true)
+        {
+            int ind_operation_begin = 0;
+            int ind_operation_end = 0;
+            int check_sum_sub = 0;
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == '+' || tmp.charAt(i) == '-' )
+                {
+                    if (i == 0)
+                        continue;
+                    check_sum_sub = i;
+                    break;
+                }
+
+            }
+            if (check_sum_sub == 0)
+                break;
+            else{
+                for (int i = check_sum_sub + 1; i < tmp.length(); i++) {
+                    if((tmp.charAt(i) == '+' || tmp.charAt(i) == '-')) {
+                        if (i - 1 == check_sum_sub)
+                            continue;
+                        ind_operation_end = i;
+                        break;
+                    }
+                }
+                if (ind_operation_end == 0)
+                    ind_operation_end = tmp.length();
+                double left;
+                left = Double.parseDouble(tmp.substring(ind_operation_begin, check_sum_sub));
+                double right = Double.parseDouble(tmp.substring(check_sum_sub + 1));
+                if (tmp.charAt(check_sum_sub) == '+')
+                {
+                    tmp = tmp.substring(0,ind_operation_begin) + (left + right)
+                            + tmp.substring(ind_operation_end);
+                }
+                else {
+                    tmp = tmp.substring(0,ind_operation_begin) + (left - right)
+                            + tmp.substring(ind_operation_end);
+                }
+
+            }
+        }
+        return tmp;
+    }
 }
--- src/test/java/by/quantumquartet/quanthink/TestBasicArithmetic.java
+++ src/test/java/by/quantumquartet/quanthink/TestBasicArithmetic.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-
+import java.lang.Math;
 import static by.quantumquartet.quanthink.math.BasicArithmetic.SolveExpression;
 public class TestBasicArithmetic {
 
@@ -17,8 +17,31 @@ public class TestBasicArithmetic {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"2+2|4", "5*9|45", "99-45|54", "44/2|22", "2*(5-1)|8","3+((14-2)*2)|27"})
+    @ValueSource(strings = {"2+2|4",
+            "5*9|45",
+            "99-45|54",
+            "44/2|22",
+            "2^4|16",
+            "14*(2-3)|-14"})
     public void testSolveExpressionSimple(String expr) {
+        int delimiter = expr.indexOf("|");
+        Object[] data = convertor(expr);
+        Assert.assertEquals((String)data[0],SolveExpression((String)data[1]));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"e^2|"+(Math.E * Math.E),
+            "2*π|"+(2*Math.PI)})
+    public void testSolveExpressionConstants(String expr) {
+        int delimiter = expr.indexOf("|");
+        Object[] data = convertor(expr);
+        Assert.assertEquals((String)data[0],SolveExpression((String)data[1]));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"(2*5^3-1000)/0.3|"+(int)((2*125-1000)/0.3)
+    })
+    public void testSolveExpressionComplex(String expr) {
         int delimiter = expr.indexOf("|");
         Object[] data = convertor(expr);
         Assert.assertEquals((String)data[0],SolveExpression((String)data[1]));
