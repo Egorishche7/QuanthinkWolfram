318a216cc4f44eb5e92ebe84c8658c1639ba0e4e
VladimirDemin <vovademin3103@gmail.com>
2024-03-24 16:53:56 +0300

    Front-end commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private msgService: MessageService
+  ) { }
+
+  ngOnInit() {
+    if (sessionStorage.getItem('email')) {
+      this.router.navigate(['/']);
+    }
+  }
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+  get password() { return this.loginForm.controls['password']; }
+
+  loginUser() {
+    const { email, password } = this.loginForm.value;
+    this.authService.getUserByEmail(email as string).subscribe(
+      response => {
+        if (response.length > 0 && response[0].password === password) {
+          sessionStorage.setItem('email', email as string);
+          this.router.navigate(['/home']);
+        } else {
+          this.msgService.add({ severity: 'error', summary: 'Error', detail: 'email or password is wrong' });
+        }
+      },
+      error => {
+        this.msgService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+      }
+
+    )
+  }
+}
