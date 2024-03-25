a1ea4d9d6b0445b34e0a5961dc43721c1dfe7a91
VladimirDemin <95424460+VladimirDemin@users.noreply.github.com>
2024-03-25 15:38:46 +0300

    Fix authorization

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,52 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { User } from 'src/app/interfaces/auth';
 import { AuthService } from 'src/app/services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
- host: { class: 'orange-form' }
+  host: { class: 'orange-form' }
 })
 export class LoginComponent implements OnInit {
-  loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required]
-  });
+  loginForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router,
     private msgService: MessageService
-  ) { }
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
     if (sessionStorage.getItem('email')) {
       this.router.navigate(['/']);
     }
   }
+
   get email() {
     return this.loginForm.controls['email'];
   }
-  get password() { return this.loginForm.controls['password']; }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
 
   loginUser() {
     const { email, password } = this.loginForm.value;
-    this.authService.getUserByEmail(email as string).subscribe(
-      response => {
+    this.authService.login({ email, password }).subscribe(
+      (response: any) => {
         if (response.length > 0 && response[0].password === password) {
-          sessionStorage.setItem('email', email as string);
+          sessionStorage.setItem('email', email);
           this.router.navigate(['/home']);
         } else {
-          this.msgService.add({ severity: 'error', summary: 'Error', detail: 'email or password is wrong' });
+          this.msgService.add({ severity: 'error', summary: 'Error', detail: 'Email or password is wrong' });
         }
       },
-      error => {
+      (error: any) => {
         this.msgService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
       }
-
-    )
+    );
   }
 }
