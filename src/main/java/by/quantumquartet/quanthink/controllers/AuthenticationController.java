9b2a6ff093a3ce3804056751100d55af10c92f76
Egorishche7 <egor.gulin04@gmail.com>
2024-05-05 20:00:29 +0300

    Реализовано подтверждение о регистарции по электронной почте

--- src/main/java/by/quantumquartet/quanthink/controllers/AuthenticationController.java
+++ src/main/java/by/quantumquartet/quanthink/controllers/AuthenticationController.java
@@ -2,13 +2,17 @@ package by.quantumquartet.quanthink.controllers;
 
 import static by.quantumquartet.quanthink.services.AppLogger.logError;
 
+import by.quantumquartet.quanthink.models.ConfirmationToken;
 import by.quantumquartet.quanthink.rest.requests.users.LoginRequest;
 import by.quantumquartet.quanthink.rest.requests.users.RegisterRequest;
 import by.quantumquartet.quanthink.rest.responses.SuccessResponse;
 import by.quantumquartet.quanthink.rest.responses.ErrorResponse;
 import by.quantumquartet.quanthink.rest.responses.jwt.JwtResponse;
+import by.quantumquartet.quanthink.rest.responses.users.UserDto;
 import by.quantumquartet.quanthink.security.JwtUtils;
 import by.quantumquartet.quanthink.models.UserDetailsImpl;
+import by.quantumquartet.quanthink.services.ConfirmationTokenService;
+import by.quantumquartet.quanthink.services.EmailService;
 import by.quantumquartet.quanthink.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,23 +22,28 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final EmailService emailService;
     private final UserService userService;
     private final JwtUtils jwtUtils;
 
     @Autowired
     public AuthenticationController(AuthenticationManager authenticationManager,
+                                    ConfirmationTokenService confirmationTokenService,
+                                    EmailService emailService,
                                     UserService userService,
                                     JwtUtils jwtUtils) {
         this.authenticationManager = authenticationManager;
+        this.confirmationTokenService = confirmationTokenService;
+        this.emailService = emailService;
         this.userService = userService;
         this.jwtUtils = jwtUtils;
     }
@@ -47,9 +56,57 @@ public class AuthenticationController {
                         .body(new ErrorResponse("Email already exists"));
             }
 
-            long newUserId = userService.registerUser(registerRequest);
+            long newUserId = userService.registerUser(registerRequest).getId();
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(new SuccessResponse<>("User registered successfully", newUserId));
+        } catch (Exception e) {
+            logError(AuthenticationController.class, e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse("Internal server error"));
+        }
+    }
+
+//    @PostMapping("/register")
+//    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
+//        if (userService.isEmailAlreadyExists(registerRequest.getEmail())) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//                    .body(new ErrorResponse("Email already exists"));
+//        }
+//        try {
+//            UserDto newUser = userService.registerUser(registerRequest);
+//
+//            ConfirmationToken confirmationToken = confirmationTokenService.createToken(newUser);
+//            emailService.sendSimpleEmail(newUser.getEmail(), confirmationToken.getToken());
+//
+//            long newUserId = newUser.getId();
+//            return ResponseEntity.status(HttpStatus.CREATED)
+//                    .body(new SuccessResponse<>("User registered successfully", newUserId));
+//        } catch (Exception e) {
+//            logError(AuthenticationController.class, e.getMessage());
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body(new ErrorResponse("Internal server error"));
+//        }
+//    }
+
+    @RequestMapping(value = "/confirmAccount", method = {RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity<?> confirmUserAccount(@RequestParam("token") String token) {
+        Optional<ConfirmationToken> confirmationTokenData = confirmationTokenService.findByToken(token);
+        if (confirmationTokenData.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse("Confirmation token not found"));
+        }
+
+        ConfirmationToken confirmationToken = confirmationTokenData.get();
+        Optional<UserDto> userData = userService.getUserById(confirmationToken.getId());
+        if (userData.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse("User not found"));
+        }
+
+        try {
+            boolean isEnabled = userService.confirmAccount(userData.get().getId());
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(new SuccessResponse<>("User account confirmed successfully", isEnabled));
         } catch (Exception e) {
             logError(AuthenticationController.class, e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
---
+++ src/main/java/by/quantumquartet/quanthink/services/EmailService.java
@@ -0,0 +1,27 @@
+package by.quantumquartet.quanthink.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Async
+    public void sendSimpleEmail(String email, String token) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Complete registration!");
+        message.setText("To confirm your account, please click here: "
+                + "http://localhost:8080/confirmAccount?token=" + token);
+        javaMailSender.send(message);
+    }
+}
